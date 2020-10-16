a4675ae284e70a796332df3b8d5d9bca59fe4e35
Marcelo Bruno <mbrunoviegas@gmail.com>
2020-10-16 17:42:16 -0300

    add new buttons and imagens for training

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from tkinter import *
 from PIL import Image, ImageTk
 from tkinter.filedialog import askopenfilename
 
+# TODO Arrumar o cancelar selecção
+# TODO Abrir diretórios
+# TODO Arrumar o zoom
+# TODO Arrumar referencia ao mexer a imagem
+# TODO Colocar os botões que faltam
+
+# (a) ler o diretório de imagens de treino/teste; 
+# (b) selecionar as características a serem usadas; ---- Botao Criado
+# (c) treinar o classificador;  ---- Botao Criado
+# (d) abrir e visualizar uma imagem; ---- FEITO
+# (e) marcar a região de interesse da imagem visualizada com o mouse; ------- FEITO
+#  (f) calcular e exibir as características para a imagem visualizada ou área selecionada;
+#  (g) classificar a imagem ou a região de interesse selecionada com o mouse.
+
 class MainWindow():
     def __init__(self):
         self.window = tk.Tk()
@@ -13,9 +27,10 @@ class MainWindow():
 
     def initUi(self):
         self.selecionando = bool(0)
-        self.ret_id = 0
         self.scale = 1
+        self.ret_id = 0
         self.btn_selecionar_text = tk.StringVar()
+        self.btn_selecionar_text.set('Selection')
         self.canvas = Canvas(width=500, height=500, bg='white')
 
         self.fr_buttons = tk.Frame(self.window, relief=tk.RAISED, bd=2)
@@ -31,8 +46,12 @@ class MainWindow():
             self.fr_buttons, text="Reset Zoom", command=self.zoom_reset)
         self.btn_selecionar = tk.Button(
             self.fr_buttons, textvariable=self.btn_selecionar_text, command=self.selecionar)
-
-        self.btn_selecionar_text.set('Selecionar Área')
+        self.btn_read_directory = tk.Button(
+            self.fr_buttons, text="Train", command=self.train)
+        self.btn_characteristics= tk.Button(
+            self.fr_buttons, text="Characteristics", command=self.characteristics)
+        self.btn_classify= tk.Button(
+            self.fr_buttons, text="Classify", command=self.classify)
 
         self.btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
         self.btn_save.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
@@ -40,6 +59,9 @@ class MainWindow():
         self.btn_zoom_out.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
         self.btn_zoom_reset.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
         self.btn_selecionar.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_read_directory.grid(row=6, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_characteristics.grid(row=7, column=0, sticky="ew", padx=5, pady=5)
+        self.btn_classify.grid(row=8, column=0, sticky="ew", padx=5, pady=5)
 
         self.fr_buttons.grid(row=0, column=0, sticky="ns")
         self.canvas.grid(row=0, column=1, sticky="nsew")
@@ -89,20 +111,18 @@ class MainWindow():
         size = int(iw * self.scale), int(ih * self.scale)
         self.photo = ImageTk.PhotoImage(self.image.resize(size))
         self.canvas.create_image(250, 250, image=self.photo)
-        if (self.ret_id != 0):
-            pos_x1 = self.x_centro - 64 * (self.scale)
-            pos_y1 = self.y_centro + 64 * (self.scale)
-            pos_x2 = self.x_centro + 64 * (self.scale)
-            pos_y2 = self.y_centro - 64 * (self.scale)
-            self.ret_id = self.canvas.create_rectangle(pos_x1, pos_y1, pos_x2, pos_y2,outline="green")
+        if (self.ret_id):
+            self.canvas.delete(self.ret_id)
+            if(self.selecionando):
+                self.drawRectangle(self.x_center, self.y_center)
 
     def selecionar(self):
         if (self.selecionando):
             self.selecionando = bool(0)
-            self.btn_selecionar_text.set('Selecionar Área')
+            self.btn_selecionar_text.set('Seletion')
         else:
             self.selecionando = bool(1)
-            self.btn_selecionar_text.set('Cancelar Seleção')
+            self.btn_selecionar_text.set('Cancel Selection')
 
     def click(self, event):
         self.canvas.scan_mark(event.x, event.y)
@@ -110,18 +130,31 @@ class MainWindow():
             self.canvas.delete(self.ret_id)
 
         if self.selecionando:
-            pos_x1 = event.x - 64 * (self.scale)
-            pos_y1 = event.y + 64 * (self.scale)
-            pos_x2 = event.x + 64 * (self.scale)
-            pos_y2 = event.y - 64 * (self.scale)
-            
-            if pos_x1 > 0 and pos_x2 < self.canvas.winfo_width() and pos_y1 > 0 and pos_y2 < self.canvas.winfo_height():
-                self.ret_id = self.canvas.create_rectangle(pos_x1, pos_y1, pos_x2, pos_y2,outline="green")
-                self.x_centro = event.x
-                self.y_centro = event.y
+            self.x_center = event.x
+            self.y_center = event.y
+            self.drawRectangle(event.x, event.y)
+
+    def drawRectangle(self, x, y):
+        pos_x1 = x - 64 * (self.scale)
+        pos_y1 = y + 64 * (self.scale)
+        pos_x2 = x + 64 * (self.scale)
+        pos_y2 = y - 64 * (self.scale)
+
+        if pos_x1 > 0 and pos_x2 < self.canvas.winfo_width() and pos_y1 > 0 and pos_y2 < self.canvas.winfo_height():
+            self.ret_id = self.canvas.create_rectangle(
+                pos_x1, pos_y1, pos_x2, pos_y2, outline="green")
 
     def drag(self, event):
         self.canvas.scan_dragto(event.x, event.y, gain=1)
+
+    def train(self):
+        pass
+    
+    def characteristics(self):
+        pass
+
+    def classify(self):
+        pass
 
 
 def main():
